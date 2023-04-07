8052228228dcdfdf7edaa2d70201423a8f553cfd
Alexey Grevtsov <grevold@yandex.ru>
2023-04-07 14:33:23 +0300

    Создал верстку формы регистрации

--- src/pages/SignUpPage/components/SignUpForm/SignUpForm.tsx
+++ src/pages/SignUpPage/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,7 @@
 import { useSignUpForm } from "./useSignUpForm";
+import s from "./SignUpForm.module.css";
+
+import { Logo } from "./../../../../components/logo/Logo"
 
 interface Props {
   onSubmit: (email: string, password: string) => void;
@@ -9,25 +12,29 @@ export const SignUpForm: React.FC<Props> = ({ onSubmit }) => {
   const { errors } = formState;
 
   return (
-    <form onSubmit={submit}>
-      <div>
-        <label>Логин</label>
-        <input {...register("email")} type="email" />
-        {errors.email?.message && <div>{errors.email?.message}</div>}
+    <form onSubmit={submit} className={s.root}>
+      <div className={s.logo}>
+        <Logo />
       </div>
-      <div>
-        <label>Пароль</label>
-        <input {...register("password")} type="password" />
-        {errors.password?.message && <div>{errors.password?.message}</div>}
+      <h1 className={s.registration_header}>Регистрация</h1>
+      <div className={s.data_row}>
+        <label className={s.label}>Логин</label>
+        <input {...register("email")} type="email" className={s.input} />
       </div>
-      <div>
-        <label>Повторить пароль</label>
-        <input {...register("repeatPassword")} type="password" />
-        {errors.repeatPassword?.message && (
-          <div>{errors.repeatPassword?.message}</div>
-        )}
+      {errors.email?.message && <div className={s.error_message}>{errors.email?.message}</div>}
+      <div className={s.data_row}>
+        <label className={s.label}>Пароль</label>
+        <input {...register("password")} type="password" className={s.input} />
       </div>
-      <button disabled={!formState.isValid} type="submit">
+      {errors.password?.message && <div className={s.error_message}>{errors.password?.message}</div>}
+      <div className={s.data_row}>
+        <label className={s.label}>Повторить пароль</label>
+        <input {...register("repeatPassword")} type="password" className={s.input} />
+      </div>
+      {errors.repeatPassword?.message && (
+        <div className={s.error_message}>{errors.repeatPassword?.message}</div>
+      )}
+      <button disabled={!formState.isValid} type="submit" className={s.button_sign_up}>
         Зарегистрироваться
       </button>
     </form>
--- src/pages/SignUpPage/components/SignUpPageContent/SignUpPageContent.tsx
+++ src/pages/SignUpPage/components/SignUpPageContent/SignUpPageContent.tsx
@@ -1,13 +1,14 @@
 import { firebaseApi } from "../../../../firebase/api";
 import { SignUpForm } from "../SignUpForm/SignUpForm";
 
+import s from "./SignUpPageContent.module.css";
+
 export const SignUpPageContent = () => {
   const handleSubmit = (email: string, password: string) =>
     firebaseApi.signUp(email, password);
 
   return (
-    <div>
-      <h2>Регистрация</h2>
+    <div className={s.root}>
       <SignUpForm onSubmit={handleSubmit} />
     </div>
   );
