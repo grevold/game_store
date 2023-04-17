a4461fc17e1eb0abedaa26fc9c0a6a0a54056104
grevold <87669945+grevold@users.noreply.github.com>
2023-04-17 16:48:21 +0300

    Добавил сообщения об ошибках в форме регистрации (#18)
    * Добавил сообщения об ошибках в sing Up firebase
    
    * Удалил console.log
    
    Удалил console.log

--- src/pages/SignUpPage/components/SignUpPageContent/SignUpPageContent.tsx
+++ src/pages/SignUpPage/components/SignUpPageContent/SignUpPageContent.tsx
@@ -1,15 +1,31 @@
 import { firebaseApi } from "../../../../firebase/api";
 import { SignUpForm } from "../SignUpForm/SignUpForm";
+import { Status, useSignUpPageContent } from "./useSignUpPageContent";
+import { texts } from "../../../../texts";
+import { Preloader } from "../../../../components/Preloader/Preloader";
 
 import s from "./SignUpPageContent.module.css";
 
+
 export const SignUpPageContent = () => {
-  const handleSubmit = (email: string, password: string) =>
-    firebaseApi.signUp(email, password);
+  const { handleFormClick, handleSubmit, state } = useSignUpPageContent();
+  const firebaseErrors: any = texts.Errors.SignUp.firebaseErrors;
+
+  if (state.status === Status.Loading) {
+    return <Preloader />;
+  }
+
 
   return (
     <div className={s.root}>
-      <SignUpForm onSubmit={handleSubmit} />
+      <SignUpForm onSubmit={handleSubmit}/>
+      {state.status === Status.Error && (
+        <span className={s.error}>
+          {firebaseErrors[state.errorCode]
+            ? firebaseErrors[state.errorCode]
+            : firebaseErrors.default}
+        </span>
+      )}
     </div>
   );
 };
