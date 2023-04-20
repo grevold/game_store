f7760a5d3ca30fad8657a7598de72d733a06ca02
Alexey Grevtsov <grevold@yandex.ru>
2023-04-20 19:16:08 +0300

    Заблокировал скролл, пока модальное окно с навигацией открыто.

--- src/components/Nav/components/NavMobile/components/ModalMenu/ModalMenu.tsx
+++ src/components/Nav/components/NavMobile/components/ModalMenu/ModalMenu.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { UserAuthStatus } from "../../../../../../types";
 import { SignOutButton } from "../../../../../SignOutButton/SignOutButton";
 import { BurgerIconClose } from "../../../../../../icons/BurgerIconClose";
+import ScrollLock from "react-scrolllock";
 
 import s from "./ModalMenu.module.css";
 
@@ -19,22 +20,24 @@ export const ModalMenu: React.FC<Props> = observer(({ className, onClose }) => {
   const routes = mapUserStateToRoutes(userState);
 
   return (
-    <div className={cn(s.root, className)}>
-      <BurgerIconClose className={s.burgerIconClose} onClick={onClose} />
-      <ul className={s.menu}>
-        {routes.map(({ title, path }) => (
-          <li>
-            <Link key={title} className={s.link} to={path} onClick={onClose}>
-              {title}
-            </Link>
-          </li>
-        ))}
-        {userState.status === UserAuthStatus.Authorized && (
-          <li>
-            <SignOutButton />
-          </li>
-        )}
-      </ul>
-    </div>
+    <ScrollLock>
+      <div className={cn(s.root, className)}>
+        <BurgerIconClose className={s.burgerIconClose} onClick={onClose} />
+        <ul className={s.menu}>
+          {routes.map(({ title, path }) => (
+            <li>
+              <Link key={title} className={s.link} to={path} onClick={onClose}>
+                {title}
+              </Link>
+            </li>
+          ))}
+          {userState.status === UserAuthStatus.Authorized && (
+            <li>
+              <SignOutButton />
+            </li>
+          )}
+        </ul>
+      </div>
+    </ScrollLock>
   );
 });
