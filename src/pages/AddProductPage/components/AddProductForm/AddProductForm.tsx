c6689604a1db5ac0e6a72ea5a73d6b72e1314215
grevold <87669945+grevold@users.noreply.github.com>
2023-04-20 23:49:02 +0300

    Протестировал форму создания товара. Данные выводятся (#25)

--- src/pages/AddProductPage/components/AddProductForm/AddProductForm.tsx
+++ src/pages/AddProductPage/components/AddProductForm/AddProductForm.tsx
@@ -1,7 +1,7 @@
-import s from "./AddProductForm.module.css";
 import { texts } from "../../../../texts";
-
 import { useAddProductForm } from "./useAddProductForm";
+
+import s from "./AddProductForm.module.css";
 
 interface Props {
   onSubmit: (
@@ -17,6 +17,7 @@ interface Props {
 export const AddProductForm: React.FC<Props> = ({ onSubmit }) => {
   const { genres } = texts.AddProduct;
   const { submit, register, formState } = useAddProductForm(onSubmit);
+
   return (
     <form className={s.root} onSubmit={submit}>
       <h1 className={s.header}>Добавление товара</h1>
@@ -26,11 +27,11 @@ export const AddProductForm: React.FC<Props> = ({ onSubmit }) => {
         <div className={s.developers}>
           <div className={s.developers_block}>
             <h2>Разработчик</h2>
-            <input className={s.input}></input>
+            <input className={s.input} {...register("developer")}></input>
           </div>
           <div className={s.developers_block}>
             <h2>Издатель</h2>
-            <input className={s.input}></input>
+            <input className={s.input} {...register("label")}></input>
           </div>
         </div>
         <h2>Жанр</h2>
@@ -47,21 +48,19 @@ export const AddProductForm: React.FC<Props> = ({ onSubmit }) => {
         <div className={s.price}>
           <div>
             <h2>Цена, ₽</h2>
-            <input className={s.input}></input>
+            <input {...register("price")} className={s.input}></input>
           </div>
-          <div>
+          {/* <div>
             <h2>Скидка, %</h2>
-            <input className={s.input}></input>
+            <input {...register("discount")} className={s.input}></input>
           </div>
           <div>
             <h2>Итог, ₽</h2>
             <input
               className={s.input_discount}
-              value={0}
-              id={"input_discount"}
-              {...register("price")}
+
             ></input>
-          </div>
+          </div> */}
         </div>
         <button className={s.button_add}>Добавить</button>
       </div>
