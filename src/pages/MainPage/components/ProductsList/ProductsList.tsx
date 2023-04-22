d477bc20b182f6a2723882685587f2e91ff17c98
grevold <87669945+grevold@users.noreply.github.com>
2023-04-23 00:28:44 +0300

    Добавил список продуктов на главной странице (#32)
    * Добавил список продуктов на главной странице
    
    * Добавил список продуктов на главной странице

--- src/pages/MainPage/components/ProductsList/ProductsList.tsx
+++ src/pages/MainPage/components/ProductsList/ProductsList.tsx
@@ -1,7 +1,38 @@
 import { useProductsList } from "./useProductsList";
+import { Preloader } from "../../../../components/Preloader/Preloader";
+import { Status } from "./useProductsList";
+import { ProductCard } from "./components/ProductCard/ProductCard";
+
+import s from "./ProductsList.module.css";
 
 export const ProductsList = () => {
-  useProductsList();
+  const { state } = useProductsList();
 
-  return <div />;
+  if (state.status === Status.Success) {
+    return (
+      <div className={s.container}>
+        <div className={s.buttons}>
+          <button className={s.button}>←</button>
+          <button className={s.button}>→</button>
+        </div>
+        <ul className={s.row_products}>
+          {state.data.map((product) => (
+            <li key={product.id} className={s.product}>
+              <ProductCard productData={product} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  if (state.status === Status.Error) {
+    return (
+      <div className={s.container}>
+        <span>Не удалось загрузить товары</span>
+      </div>
+    );
+  }
+
+  return <Preloader />;
 };
--- src/pages/MainPage/components/ProductsList/useProductsList.ts
+++ src/pages/MainPage/components/ProductsList/useProductsList.ts
@@ -1,8 +1,33 @@
 import { useEffect } from "react";
 import { firebaseApi } from "../../../../firebase/api";
+import { useState } from "react";
+import { Product } from "../../../../types";
+
+export enum Status {
+  Init = "Init",
+  Loading = "Loading",
+  Success = "Success",
+  Error = "Error",
+}
+
+type State =
+  | {
+      status: Status.Loading | Status.Error;
+    }
+  | {
+      status: Status.Success;
+      data: Product[];
+    };
 
 export function useProductsList() {
+  const [state, setState] = useState<State>({
+    status: Status.Loading,
+  });
   useEffect(() => {
-    firebaseApi.fetchAllProducts().then((data) => console.log(data));
+    firebaseApi.fetchAllProducts().then(
+      (data) => setState({ status: Status.Success, data: [...data] }),
+      () => setState({ status: Status.Error })
+    );
   }, []);
+  return { state };
 }
