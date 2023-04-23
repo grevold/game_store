b4a3fe2378512992e0585473a0c9eb02631feded
Alexey Grevtsov <grevold@yandex.ru>
2023-04-24 00:18:27 +0300

    Добавил SwiperSlider

--- src/pages/MainPage/components/ProductsList/ProductsList.tsx
+++ src/pages/MainPage/components/ProductsList/ProductsList.tsx
@@ -1,11 +1,8 @@
 import { useProductsList } from "./useProductsList";
 import { Preloader } from "../../../../components/Preloader/Preloader";
 import { Status } from "./useProductsList";
-import { ProductCard } from "./components/ProductCard/ProductCard";
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import { ProductsSlider } from "./components/ProductsSlider/ProductsSlider";
 import "swiper/css";
-import "swiper/css/pagination";
 
 import s from "./ProductsList.module.css";
 
@@ -13,26 +10,7 @@ export const ProductsList = () => {
   const { state } = useProductsList();
 
   if (state.status === Status.Success) {
-    return (
-      <Swiper
-        breakpoints={{
-          400: {
-            slidesPerView: 1,
-            spaceBetween: 50,
-          },
-          999: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-      >
-        {state.data.map((product) => (
-          <SwiperSlide>
-            <ProductCard productData={product} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    );
+    return <ProductsSlider products={state.products} />;
   }
 
   if (state.status === Status.Error) {
