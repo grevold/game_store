47d903a5f564d3c65b0f38c52d644a22197aa6d9
grevold <87669945+grevold@users.noreply.github.com>
2023-05-09 13:29:12 +0300

    Стилизовал стрелочки для слайдера (#38)
    * Добавил стили к кнопке Добавить в корзину в карточке товара
    
    * Стилизовал кнопку добавления товаров в корзину
    
    * Стилизовал кнопку добавления товаров в корзину
    
    * Стилизовал кнопку добавления товаров в корзину
    
    * Стилизовал стрелочки для слайдера
    
    * Стилизовал стрелочки для слайдера

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,3 @@
-import { Preloader } from "../../components/Preloader/Preloader";
 import { ProductsList } from "./components/ProductsList/ProductsList";
 
 import s from "./MainPage.module.css";
@@ -6,7 +5,6 @@ import s from "./MainPage.module.css";
 export const MainPage = () => {
   return (
     <div className={s.root}>
-      <h1>Главная страница</h1>
       <ProductsList />
     </div>
   );
--- src/pages/MainPage/components/ProductsList/components/ProductsSlider/useProductSlider.ts
+++ src/pages/MainPage/components/ProductsList/components/ProductsSlider/useProductSlider.ts
@@ -7,7 +7,14 @@ export function useProductSlider() {
     if (!buttonPrev || !buttonNext) {
       return;
     }
-    buttonPrev.innerHTML = "<";
-    buttonNext.innerHTML = ">";
+
+    buttonPrev.innerHTML = `<svg width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="36" cy="36" r="35" stroke="white" stroke-width="2"/>
+    <path d="M42 24L28 36.5L42 49" stroke="white" stroke-width="2" stroke-linecap="round"/>
+    </svg>`;
+    buttonNext.innerHTML = `<svg width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="36" cy="36" r="35" stroke="white" stroke-width="2" />
+    <path d="M32 49L46 36.5L32 24" stroke="white" stroke-width="2" stroke-linecap="round" />
+</svg>`;
   }, []);
 }
