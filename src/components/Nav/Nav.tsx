21b72435339eb14252ab315303b42629c6a3076d
Alexey Grevtsov <grevold@yandex.ru>
2023-05-23 16:55:15 +0500

    Добавил счет товаров в корзине

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,13 @@
-import { store } from "../../store";
-import { observer } from "mobx-react-lite";
 import { NavDesktop } from "./components/NavDesktop/NavDesktop";
 import { NavMobile } from "./components/NavMobile/NavMobile";
 
 import s from "./Nav.module.css";
 
-export const Nav = observer(() => {
-  const userState = store.getUserState();
-
+export const Nav = () => {
   return (
     <>
       <NavDesktop className={s.navDesktop} />
       <NavMobile className={s.navMobile} />
     </>
   );
-});
+};
