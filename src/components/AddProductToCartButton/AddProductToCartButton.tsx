ef191a4010df8044bb6fbb57fb8431d1794ea6e0
grevold <87669945+grevold@users.noreply.github.com>
2023-06-04 09:05:54 +0300

    Добавил изображения товаров (#66)

--- src/components/AddProductToCartButton/AddProductToCartButton.tsx
+++ src/components/AddProductToCartButton/AddProductToCartButton.tsx
@@ -10,58 +10,57 @@ interface Props {
   productId: string;
   className?: string;
 }
+const Component = ({ productId, className }: Props) => {
+  const userState = store.getUserState();
+  const { state, addProductToCart } = useAddProductToCartButton(
+    userState,
+    productId
+  );
 
-export const AddProductToCartButton: React.FC<Props> = observer(
-  ({ productId, className }) => {
-    const userState = store.getUserState();
-    const { state, addProductToCart } = useAddProductToCartButton(
-      userState,
-      productId
-    );
-
-    if (state.status === Status.NeedToSignIn) {
-      return (
-        <Link to={RoutePath.SignInPage}>
-          <button className={className}>В корзину</button>
-        </Link>
-      );
-    }
-
-    if (state.status === Status.Init) {
-      return (
-        <button onClick={addProductToCart} className={className}>
-          В корзину
-        </button>
-      );
-    }
-
-    if (state.status === Status.Error) {
-      return (
-        <>
-          <button onClick={addProductToCart} className={className}>
-            В корзину
-          </button>
-          <span className={s.message_error}>
-            Произошла ошибка при добавлении товара в корзину. Пожалуйста,
-            повторите попытку позже.
-          </span>
-        </>
-      );
-    }
-
-    if (state.status === Status.Success) {
-      return (
-        <Link to={RoutePath.CartPage} className={s.link}>
-          <button onClick={addProductToCart} className={s.button_go_to_cart}>
-            Перейти в корзину
-          </button>
-        </Link>
-      );
-    }
+  if (state.status === Status.NeedToSignIn) {
     return (
-      <button onClick={addProductToCart} className={s.button_go_to_cart}>
-        <Preloader className={s.preloader} />
+      <Link to={RoutePath.SignInPage}>
+        <button className={className}>В корзину</button>
+      </Link>
+    );
+  }
+
+  if (state.status === Status.Init) {
+    return (
+      <button onClick={addProductToCart} className={className}>
+        В корзину
       </button>
     );
   }
-);
+
+  if (state.status === Status.Error) {
+    return (
+      <>
+        <button onClick={addProductToCart} className={className}>
+          В корзину
+        </button>
+        <span className={s.message_error}>
+          Произошла ошибка при добавлении товара в корзину. Пожалуйста,
+          повторите попытку позже.
+        </span>
+      </>
+    );
+  }
+
+  if (state.status === Status.Success) {
+    return (
+      <Link to={RoutePath.CartPage} className={s.link}>
+        <button onClick={addProductToCart} className={s.button_go_to_cart}>
+          Перейти в корзину
+        </button>
+      </Link>
+    );
+  }
+  return (
+    <button onClick={addProductToCart} className={s.button_go_to_cart}>
+      <Preloader className={s.preloader} />
+    </button>
+  );
+};
+
+export const AddProductToCartButton: React.FC<Props> = observer(Component);
