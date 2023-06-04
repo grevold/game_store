3bc5d25a283c914857d85b16ca96cd9a4bc4015c
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-06-04 21:45:43 +0300

    Реализовал базовую логику для страницы каталог (#69)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/pages/CatalogPage/components/CatalogPageContent/CatalogPageContent.tsx
+++ src/pages/CatalogPage/components/CatalogPageContent/CatalogPageContent.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { Filter } from "../Filter/Filter";
 import { ProductsList } from "../ProductsList/ProductsList";
 import s from "./CatalogPageContent.module.css";
+import { Product } from "../../../../types";
+import { observer } from "mobx-react-lite";
+import { store } from "../../../../store";
 
-export function CatalogPageContent() {
+export const CatalogPageContent = observer(() => {
+  const allProducts = store.getProductsState().products;
+  const [products, setProducts] = useState<Product[]>(allProducts);
+
+  const handleFilterApply = (
+    queryString: string,
+    maxPrice: number,
+    genre: string
+  ) => {
+    const products = allProducts.filter((product) => product.genre === genre);
+    setProducts(products);
+  };
+
   return (
     <div className={s.root}>
-      <Filter />
-      <ProductsList />
+      <Filter handleFilterApply={handleFilterApply} />
+      <ProductsList products={products} />
     </div>
   );
-}
+});
--- src/pages/CatalogPage/components/ProductsList/ProductsList.tsx
+++ src/pages/CatalogPage/components/ProductsList/ProductsList.tsx
@@ -1,5 +1,17 @@
+import { Product } from "../../../../types";
+
 import s from "./ProductsList.module.css";
 
-export function ProductsList() {
-  return <div className={s.root}></div>;
+interface Props {
+  products: Product[];
+}
+
+export function ProductsList({ products }: Props) {
+  return (
+    <div className={s.root}>
+      {products.map(({ id, name }) => (
+        <div key={id}>{name}</div>
+      ))}
+    </div>
+  );
 }
