b2cb62224cc3a232f6c6932f5177af6a8fa9bfbb
grevold <87669945+grevold@users.noreply.github.com>
2023-06-05 23:02:16 +0500

    Добавил поиск по названиям в фильтре (#73)
    * Добавил поиск по названиям в фильтре
    
    * Добавил поиск по названиям в фильтре

--- src/pages/CatalogPage/components/CatalogPageContent/CatalogPageContent.tsx
+++ src/pages/CatalogPage/components/CatalogPageContent/CatalogPageContent.tsx
@@ -10,12 +10,32 @@ export const CatalogPageContent = observer(() => {
   const allProducts = store.getProductsState().products;
   const [products, setProducts] = useState<Product[]>(allProducts);
 
+  function isGameNameSatisfySearchString(
+    searchString: string,
+    gameName: string
+  ) {
+    const string1 = searchString.toLowerCase();
+    const string2 = gameName.toLowerCase();
+    return string2.startsWith(string1);
+  }
+
+  function findGenreGame(searchGenre: string, gameGenre: string) {
+    if (searchGenre === "Все") {
+      return true;
+    }
+    return searchGenre === gameGenre;
+  }
+
   const handleFilterApply = (
     queryString: string,
     maxPrice: number,
     genre: string
   ) => {
-    const products = allProducts.filter((product) => product.genre === genre);
+    const products = allProducts.filter(
+      (product) =>
+        findGenreGame(genre, product.genre) &&
+        isGameNameSatisfySearchString(queryString, product.name)
+    );
     setProducts(products);
   };
 
