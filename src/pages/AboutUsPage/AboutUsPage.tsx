e108a6e3e705ee41bcb51d87d435487ebfe3e6f1
Алексей <87669945+grevold@users.noreply.github.com>
2023-09-08 11:40:25 +0300

    Добавил тестовый музыкальный блок (#83)

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -5,10 +5,12 @@ import { Values } from "./components/Values/Values";
 
 import s from "./AboutUsPage.module.css";
 import { Film } from "./components/Film/Film";
+import { Music } from "./components/Music/Music";
 
 export function AboutUsPage() {
   return (
     <div className={s.root}>
+      <Music />
       <Intro />
       <History />
       <Advantages />
