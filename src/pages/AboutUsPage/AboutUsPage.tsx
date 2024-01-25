52aaf2398916d3d42ec9b91a0239ba48bd90a32e
Алексей <87669945+grevold@users.noreply.github.com>
2024-01-25 11:53:32 +0300

    добавил мета-даные (#90)

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -5,17 +5,15 @@ import { Values } from "./components/Values/Values";
 
 import s from "./AboutUsPage.module.css";
 import { Film } from "./components/Film/Film";
-import { Pads } from "./components/Pads/Pads";
 
 export function AboutUsPage() {
   return (
     <div className={s.root}>
-      <Pads />
-      {/* <Intro />
+      <Intro />
       <History />
       <Advantages />
       <Values />
-      <Film /> */}
+      <Film />
     </div>
   );
 }
